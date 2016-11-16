facc7fb344d06a1b8c073e5132c1de4f71f530be
nikhil027 <nikhil.sim@gmail.com>
2016-11-16 10:46:39 +0530

    properties_controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -45,7 +45,7 @@ class PropertiesController < ApplicationController
 
 private
 	def property_params
-		params[:property].permit(:name,:type,:built_up_area,:owner_id,:is_available,:property_reserve,:description,:country_id,:state_id,:city_id,:address,:water_bill_details,:electricity_bill_details)
+		params[:property].permit(:name,:property_type,:built_up_area,:owner_id,:is_available,:property_reserve,:description,:country_id,:state_id,:city_id,:address,:water_bill_details,:electricity_bill_details)
 	end
 
 end
