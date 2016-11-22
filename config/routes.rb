53b27dc2057e266e092338d28b429cf88a78aab1
nikhil027 <nikhil.sim@gmail.com>
2016-11-22 10:21:28 +0530

    user_creation_through_crud

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  scope "/admin" do
+    resources :users
+  end
+
+  resources :dashboard
   resources :tasks
   resources :properties
   resources :userdetails
