1e1b3103aa0b47cf5a9ea34eca55d75e315cd1d5
nikhil027 <nikhil.sim@gmail.com>
2016-12-12 18:21:28 +0530

    removed_col_from_properties

--- db/migrate/20161207175317_add_property_documents_and_property_pictures_to_properties.rb
+++
@@ -1,6 +0,0 @@
-class AddPropertyDocumentsAndPropertyPicturesToProperties < ActiveRecord::Migration
-  def change
-    add_column :properties, :property_documents, :json
-    add_column :properties, :property_pictures, :json
-  end
-end
--- db/migrate/20161207182221_add_column_documents_and_pictures_to_properties.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnDocumentsAndPicturesToProperties < ActiveRecord::Migration
-  def change
-    add_column :properties, :documents, :array
-    add_column :properties, :pictures, :array
-  end
-end
